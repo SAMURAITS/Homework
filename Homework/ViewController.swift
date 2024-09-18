88e0b2d2da6207fa11696b3e3cd77cbf982880a4
Timofey <tima_spirin21000@mail.ru>
2024-09-19 01:44:30 +0500

    Создал счётчик

--- Homework/ViewController.swift
+++ Homework/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var label: UILabel!
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTap(_ sender: Any) {
+        count += 1
+        label.text = String(count)
+    }
+    
 }
 
